d7d16767fccffbbf02603658ce3520a0c7f9862b
Rich <richard.schenkel@hs-osnabrueck.de>
2025-06-04 16:36:18 +0200

    Admin can Add Pizzas

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
-INSERT INTO Pizza (id, name, beschreibung, preis) VALUES (1, 'Margherita', 'Klassiker mit Mozzarella', 7.50);
-INSERT INTO Pizza (id, name, beschreibung, preis) VALUES (2, 'Salami', 'Mit scharfer Rindersalami', 8.90);
-INSERT INTO Pizza (id, name, beschreibung, preis) VALUES (3, 'Funghi', 'Mit frischen Pilzen', 8.20);
-INSERT INTO Pizza (id, name, beschreibung, preis) VALUES (4, 'Hawaii', 'Mit Schinken und Ananas', 9.50);
+INSERT INTO Pizza (id, name, beschreibung, preis) VALUES (50, 'Margherita', 'Klassiker mit Mozzarella', 7.50);
+INSERT INTO Pizza (id, name, beschreibung, preis) VALUES (20, 'Salami', 'Mit scharfer Rindersalami', 8.90);
+INSERT INTO Pizza (id, name, beschreibung, preis) VALUES (30, 'Funghi', 'Mit frischen Pilzen', 8.20);
+INSERT INTO Pizza (id, name, beschreibung, preis) VALUES (40, 'Hawaii', 'Mit Schinken und Ananas', 9.50);
 
 -- ODER: mit Adresse (falls Du Adresse setzen möchtest)
 INSERT INTO kunde(vorname, nachname, email)
